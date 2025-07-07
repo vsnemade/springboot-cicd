dba0e4e25fd2915da0ebcfd324b20a9d68d8f0fb
Vishal Nemade <vsnemade@gmail.com>
2025-07-07 08:20:37 +0530

    first commit

---
+++ src/main/java/com/vishtech/order/SpringBootCicd.java
@@ -0,0 +1,41 @@
+package com.vishtech.order;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@SpringBootApplication
+@RestController
+@Slf4j
+public class SpringBootCicd {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCicd.class, args);
+    }
+
+    @GetMapping("/")
+    public String welcome(){
+        log.info("Request received for '/'");
+        return "Welcome at "+LocalDateTime.now();
+    }
+
+
+    @GetMapping("/hello")
+    public String hello(){
+        log.info("Request received for '/hello'");
+        return "Hello at "+LocalDateTime.now();
+    }
+
+    @GetMapping("/health")
+    public ResponseEntity<String> health(){
+        log.info("Request received for '/health'");
+        return ResponseEntity.ok("Demo Application is healthy");
+    }
+
+}
